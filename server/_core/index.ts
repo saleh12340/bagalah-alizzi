75034822d91ebe283a5c0501cca65cad16d41671
saleh12340 <salthbiany@gmail.com>
2026-09-19 23:23:39 +0300

    remove unavailable demo database initializer

--- server/_core/index.ts
+++ server/_core/index.ts
@@ -7,7 +7,6 @@ import { registerOAuthRoutes } from "./oauth";
 import { registerStorageProxy } from "./storageProxy";
 import { appRouter } from "../routers";
 import { createContext } from "./context";
-import { ensureDemoData } from "../db";
 
 function isPortAvailable(port: number): Promise<boolean> {
   return new Promise((resolve) => {
@@ -31,8 +30,6 @@ async function findAvailablePort(startPort: number = 3000): Promise<number> {
 async function startServer() {
   // On a fresh database, prepare realistic demo inventory and customer accounts.
   // Existing store data is never overwritten.
-  await ensureDemoData();
-
   const app = express();
   const server = createServer(app);
 
