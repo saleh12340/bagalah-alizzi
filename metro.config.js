5b07b1d25257347944dda51c4f16d260ca138ad4
saleh12340 <salthbiany@gmail.com>
2026-09-20 13:49:23 +0300

    Delete metro.config.js

--- metro.config.js
+++
@@ -1,4 +0,0 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const config = getDefaultConfig(__dirname);
-if (!config.resolver.assetExts.includes("html")) config.resolver.assetExts.push("html");
-module.exports = config;
